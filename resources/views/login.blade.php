ca30b0104d82695f5bb0e6fc5abd3dffb2685b13
Arzxy <akmalrizki0102@gmail.com>
2025-03-22 14:29:11 +0700

    Add login functionality with LoginController and login view

---
+++ resources/views/login.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>SiBapDos</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+        <meta content="Coderthemes" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- App css -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body class="authentication-bg authentication-bg-pattern d-flex align-items-center">
+        <div class="account-pages w-100 mt-5 mb-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="card">
+
+                            <div class="card-body p-4">
+                                
+                                <div class="text-center mb-4">
+                                    <a href="index.html">
+                                        <span><img src="assets/images/logo-dark.png" alt="" height="28"></span>
+                                    </a>
+                                </div>
+
+                                <form action="#" class="pt-2">
+
+                                    <div class="form-group mb-3">
+                                        <label for="emailaddress">NIM/NIDN</label>
+                                        <input class="form-control" type="text" name="nimnidn" required="" placeholder="Enter your NIM or NIDN">
+                                    </div>
+
+                                    <div class="form-group mb-3">
+                                        <label for="password">Password</label>
+                                        <input class="form-control" type="password" required="" name="password" placeholder="Enter your Password">
+                                    </div>
+
+                                    <div class="form-group mb-0 text-center">
+                                        <button class="btn btn-success btn-block" type="submit"> Log In </button>
+                                    </div>
+
+                                </form>
+
+                                <!-- end row -->
+
+                            </div> <!-- end card-body -->
+                        </div>
+                        <!-- end card -->
+
+                    </div> <!-- end col -->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end page -->
+        
+
+        <!-- Footer Start -->
+        <footer class="footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        Copyright © 2025 SiBapDos
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end Footer -->
+         
+        <!-- Vendor js -->
+        <script src="assets/js/vendor.min.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/app.min.js"></script>
+        
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\IndexController;
-
-use App\Http\Controllers\MahasiswaController;
-use App\Http\Controllers\ProdiController;
-use App\Http\Controllers\MatakuliahController;
-use App\Http\Controllers\NilaiController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,19 +17,4 @@ use App\Http\Controllers\NilaiController;
 */
 
 Route::get('/', [IndexController::class, 'index']);
-
-Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
-Route::get('/mahasiswa_insert', [MahasiswaController::class, 'insert']);
-Route::get('/mahasiswa_edit', [MahasiswaController::class, 'edit']);
-
-Route::get('/prodi', [ProdiController::class, 'index']);
-Route::get('/prodi_insert', [ProdiController::class, 'insert']);
-Route::get('/prodi_edit', [ProdiController::class, 'edit']);
-
-Route::get('/matakuliah', [MatakuliahController::class, 'index']);
-Route::get('/matakuliah_insert', [MatakuliahController::class, 'insert']);
-Route::get('/matakuliah_edit', [MatakuliahController::class, 'edit']);
-
-Route::get('/nilai', [NilaiController::class, 'index']);
-Route::get('/nilai_insert', [NilaiController::class, 'insert']);
-Route::get('/nilai_edit', [NilaiController::class, 'edit']);
+Route::get('/login', [LoginController::class, 'index']);
